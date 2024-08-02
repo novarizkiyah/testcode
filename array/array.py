1171f045007b5bd75b263d2c21cee85b4dba68ac
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-08-02 14:26:00 +0200

    2 Agustus

---
+++ array/array.py
@@ -0,0 +1,40 @@
+
+
+numbbers = [0, -2, -3, -5, 1, 3, 4, 6, 7, 5, 2]
+
+def find_max(numbbers):
+    # Inisialisasi nilai maksimum dengan nilai elemen pertama dalam array
+    max_number = numbbers[0]
+
+    # Loop melalui setiap elemen dalam array
+    for x in numbbers:
+        # Jika elemen saat ini lebih besar dari max_number, perbarui max_number
+        if x > max_number:
+            max_number = x
+    return max_number
+
+result = find_max(numbbers)
+print(result)
+
+def find_min(numbbers):
+    min_number=numbbers[0]
+
+    for z in numbbers:
+        if z < min_number:
+            min_number = z
+    return min_number
+
+result2 = find_min(numbbers)
+print(result2)
+
+
+def count_positife(numbbers):
+    count =numbbers[0]
+
+    for x in numbbers:
+        if x>0:
+            count+=1
+    return count
+
+result3=count_positife(numbbers)
+print(result3)
