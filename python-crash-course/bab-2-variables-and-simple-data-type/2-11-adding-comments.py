084ca4668bcf2d11d9ee6391704ec5fb4405eb94
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-26 18:19:22 +0200

    last of bab 2

---
+++ python-crash-course/bab-2-variables-and-simple-data-type/2-11-adding-comments.py
@@ -0,0 +1,6 @@
+#add variables represent your favorite number
+my_favorite_number = 7
+# create message that reveal your favorite number
+message = f"My favorote number is {my_favorite_number}"
+#print that message
+print(message)
