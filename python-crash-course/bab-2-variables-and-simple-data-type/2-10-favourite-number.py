7af7c3fa84b1dbdd0e942849366c8fd8eaa2110c
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-26 18:01:45 +0200

    favourite number

---
+++ python-crash-course/bab-2-variables-and-simple-data-type/2-10-favourite-number.py
@@ -0,0 +1,8 @@
+#my_favorite_number = 7
+#message = "What is my favourite number? my favorite number is "
+
+#print(f"{message} {my_favorite_number} ")
+
+my_favorite_number = 7
+message = f"My favorote number is {my_favorite_number}"
+print(message)
