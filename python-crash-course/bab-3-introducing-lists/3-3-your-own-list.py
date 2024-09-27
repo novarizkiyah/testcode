b0ca54715e3b3c495f954cfd2ac688f7a47ca080
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-27 19:11:58 +0200

    own list

---
+++ python-crash-course/bab-3-introducing-lists/3-3-your-own-list.py
@@ -0,0 +1,4 @@
+transportations = ['motorcycle', 'car', 'train']
+print(f"I would like to own a {transportations[0]}")
+print(f"I would like to own a {transportations[1]}")
+print(f"I would like to own a {transportations[2]}")
