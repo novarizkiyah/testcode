4c74d5dc825a4f158855eef84bcdc14675f27ea9
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-27 19:18:06 +0200

    guest list

---
+++ python-crash-course/bab-3-introducing-lists/3-4-guest-list.py
@@ -0,0 +1,4 @@
+guest_list = ['nova', 'aya', 'aryya']
+print(f"Do you want to dinner with me, dear {guest_list[0].title()}")
+print(f"Do you want to dinner with me, dear {guest_list[1].title()}")
+print(f"Do you want to dinner with me, dear {guest_list[2].title()}")
