c64f55d3e1567a1a8c9eb18d8c63e692251a1a7a
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-27 20:01:15 +0200

    shrink guest list

--- python-crash-course/bab-3-introducing-lists/3-7-shrink-guest-list.py
+++ python-crash-course/bab-3-introducing-lists/3-7-shrink-guest-list.py
@@ -7,9 +7,29 @@ print(guest_list)
 guest_list.insert(0, 'mulyono')
 guest_list.insert(3,'fufufafa')
 guest_list.append('rk')
+print(guest_list)
+
+print(f"Sorry helemaal, I just have 2 seats for dinner")
+last_guest = guest_list.pop(5)
+print(last_guest)
+print(f"Sorry, {last_guest.title()} I can not invite you to my dinner")
+
+last_second_guest = guest_list.pop(4)
+print(last_second_guest)
+print(f"Sorry, {last_second_guest.title()} I can not invite you to my dinner")
+
+last_third_guest = guest_list.pop(3)
+print(last_third_guest)
+print(f"Sorry, {last_third_guest.title()} I can not invite you to my dinner")
+
+last_fourth_guest = guest_list.pop(2)
+print(last_fourth_guest)
+print(f"Sorry, {last_fourth_guest.title()} I can not invite you to my dinner")
+
+print(guest_list)
 print(f"Do you want to dinner with me, dear {guest_list[0].title()}")
 print(f"Do you want to dinner with me, dear {guest_list[1].title()}")
-print(f"Do you want to dinner with me, dear {guest_list[2].title()}")
-print(f"Do you want to dinner with me, dear {guest_list[3].title()}")
-print(f"Do you want to dinner with me, dear {guest_list[4].title()}")
-print(f"Do you want to dinner with me, dear {guest_list[5].title()}")
+
+del guest_list[0]
+del guest_list[0]
+print(guest_list)
