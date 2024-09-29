4222446ec50a19a2dacd71cfaffe8a19f27cc875
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-29 15:01:05 +0200

    Using len to know how much people

---
+++ python-crash-course/bab-3-introducing-lists/3-9-dinner-guests.py
@@ -0,0 +1,8 @@
+# working with one of the pogram from exercises 3-4 -3.7, use len() to print message indicating a number pf people you're inviting dinner
+
+guest_list = ['nova', 'aya', 'aryya']
+print(guest_list)
+guest_list[0] = 'rizki'
+print(guest_list)
+how_much = len(guest_list)
+print(f"So, I will inviting {how_much} people")
