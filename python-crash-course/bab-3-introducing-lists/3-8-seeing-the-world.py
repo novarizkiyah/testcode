5f916f0fc5c87ab3ae060bc62ee7ffbe063ec452
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-29 11:38:28 +0200

    add reverse but i don't know how

--- python-crash-course/bab-3-introducing-lists/3-8-seeing-the-world.py
+++ python-crash-course/bab-3-introducing-lists/3-8-seeing-the-world.py
@@ -10,3 +10,16 @@ print(my_go_to_places)
 print(sorted(my_go_to_places))
 
 #4. show that your list is still in its original order by printing it.
+print(my_go_to_places)
+
+#5. use sorted() to print your list in reverse-alphabetical order without changing the order of the original list
+print(sorted(my_go_to_places.reverse))
+#6. show that your list is still in its original order by printing it again
+print(my_go_to_places)
+#7. Use reverse() to change the order of your list. Print the list to show that its order has changed
+
+#8. Use reverse() to change the order of your list. Print the list to show it's back to its original order.
+
+#9. Use sort() to change your list so it's stored in alphabetical order. Print the list to show that its order has been changed.
+
+#10. Use sort() to change your list so it's stored in reverse-alphabetical order. Ptint the list to show that its order has changed.
