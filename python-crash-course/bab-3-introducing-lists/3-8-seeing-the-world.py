fbecc88c63971cdf411e11741705285983122751
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-29 09:21:17 +0200

    seeing the world

---
+++ python-crash-course/bab-3-introducing-lists/3-8-seeing-the-world.py
@@ -0,0 +1,12 @@
+# Seeing the World : Think of at least 5 places in the world you'd like to visit
+
+# 1. store the location in a list. Make sure the list is not alphabetical order.
+my_go_to_places = ['madinah', 'paris', 'gowa', 'washington', 'jogjakarta']
+
+#2. print your list in its original order. Don't worry about printing the list neatly; just print it as a raw Python list.
+print(my_go_to_places)
+
+#3. use sorted() to print your list in alphabetical order without modifying the actual list
+print(sorted(my_go_to_places))
+
+#4. show that your list is still in its original order by printing it.
