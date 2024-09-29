17cab0244b18fe8c73e97a9f923848a8d0d645eb
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-09-29 16:48:43 +0200

    add function

---
+++ python-crash-course/bab-3-introducing-lists/3-10-every-function.py
@@ -0,0 +1,8 @@
+# Store in the list and use function
+
+languages = ['dutch', "javanese", "indonesia", "japanese", "engish"]
+
+#Make function
+
+print(sorted(languages))
+print(sorted(languages, reverse=True))
