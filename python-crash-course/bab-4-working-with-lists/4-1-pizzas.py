8619743cf72eec6b3f8e0b2748ece00713781ee0
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-01 10:30:43 +0200

    Add bab 4

---
+++ python-crash-course/bab-4-working-with-lists/4-1-pizzas.py
@@ -0,0 +1,6 @@
+# Think 3 kinds of your favorite pizza. Store pizza in a lists the use loop to print each pizza.
+pizzas = ['enak','enak_aja', 'enak_banget']
+for pizza in pizzas:
+    print(pizza)
+
+# 
