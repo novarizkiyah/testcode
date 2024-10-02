477e68d964d4bc7fbadc4f0ccc6d973a36f52287
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-02 14:24:26 +0200

    abut pizza

--- python-crash-course/bab-4-working-with-lists/4-1-pizzas.py
+++ python-crash-course/bab-4-working-with-lists/4-1-pizzas.py
@@ -3,4 +3,11 @@ pizzas = ['enak','enak_aja', 'enak_banget']
 for pizza in pizzas:
     print(pizza)
 
-# 
+# bikin sentences dalam for loop, contoh i like -nama pizza- pizza
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza")
+
+# add line outside the loop I really love pizza
+
+print("I really love pizza")
