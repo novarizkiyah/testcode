9f1487c0f48497c4ab6b90026cc418d8164bf2af
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-04 23:43:01 +0200

    Animals

---
+++ python-crash-course/bab-4-working-with-lists/4-2-1animals.py
@@ -0,0 +1,13 @@
+# Use 3 animals list and print
+
+animals = ['cow', 'chicken', 'pig']
+for animal in animals:
+    print (animal)
+
+# modify the print statemen
+
+animals = ['cow', 'chicken', 'pig']
+for animal in animals:
+    print(f"a {animal} would be a great pet")
+#add a line in program 
+print("Any of these animals wpuld make a great pet")
