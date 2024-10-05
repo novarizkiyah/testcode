ef0bd5b73fc09ef5b6ca7b1038ad66eb4378a663
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-05 21:09:29 +0200

    Min Max Value

--- python-crash-course/bab-4-working-with-lists/4-4-one-million.py
+++ python-crash-course/bab-4-working-with-lists/4-4-one-million.py
@@ -1,4 +1,8 @@
 #make a list to 1 until one million
 
-for value in range (1, 1000001):
-    print(value)
+#for value in range (1, 1000001):
+#    print(value)
+
+#make a list to 1 until one million
+millions = list(range(1,1000001))
+print(millions)
--- python-crash-course/bab-4-working-with-lists/4-5-summing-a-million.py
+++ python-crash-course/bab-4-working-with-lists/4-5-summing-a-million.py
@@ -1,9 +1,13 @@
 #Summing million 
 #make a list 1 to million
-for value in range (1, 1000001):
-    print(value)
+millions = list(range(1,1000001))
+print(millions)
 
 #use min() and max() to make sure first and last number right
-
-min_values= min(value)
-print(min_values)
+min_value = min(millions)
+print(f"Max falue is {min_value}")
+max_value = max(millions)
+print(f"Max falue is {max_value}")
+#use sum() to sum
+sum_value = sum(millions)
+print(f"Sum value is {sum_value}")
