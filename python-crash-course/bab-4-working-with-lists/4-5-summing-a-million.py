8687200397eeb9c2cda3b3ef691acbffc76dba43
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-05 21:03:41 +0200

    Not using loop, but list

--- python-crash-course/bab-4-working-with-lists/4-5-summing-a-million.py
+++ python-crash-course/bab-4-working-with-lists/4-5-summing-a-million.py
@@ -2,3 +2,8 @@
 #make a list 1 to million
 for value in range (1, 1000001):
     print(value)
+
+#use min() and max() to make sure first and last number right
+
+min_values= min(value)
+print(min_values)
