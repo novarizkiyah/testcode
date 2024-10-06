4f27ad26f9d9c105008b513aa51c2f10d7c405f3
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-06 20:51:12 +0200

    Slices

---
+++ python-crash-course/bab-4-working-with-lists/4-10-slices.py
@@ -0,0 +1,9 @@
+#Using one of the progran you wrote in this chapter, add several lines to the end of the program that do the following
+animals = ['cow', 'chicken', 'pig', 'panda', 'monkey']
+
+# Print the messsage 
+print("The first three items in the list are : ")
+
+#Then using slices to print first three item from that program list
+
+print(animals[0:3])
