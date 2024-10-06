2efb2a4eda57020970287f48b17c8c602310aa8b
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-06 09:31:21 +0200

    Odd number

---
+++ python-crash-course/bab-4-working-with-lists/4-6-odd-number.py
@@ -0,0 +1,8 @@
+#Use the third argument of range() function to make a list of the odd number from 1 to 20. 
+
+odd_numbers = list(range(1,21, 2))
+print(odd_numbers)
+
+#use loop for each number
+for odd_number in (odd_numbers):
+    print(odd_number)
