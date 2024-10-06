299a22aee8e3f1d503dbb2162dc06ff41478b436
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-06 09:39:34 +0200

    Multiple three

---
+++ python-crash-course/bab-4-working-with-lists/4-7-threes.py
@@ -0,0 +1,9 @@
+# Make a list of multiple of 3, from 3 to 30
+
+multiple_3 = list(range(3,33,3))
+print(multiple_3)
+
+#Use a for loop to print the numbers in your list
+
+for value in (multiple_3):
+    print(value)
