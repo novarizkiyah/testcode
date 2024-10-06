2a6a81c0ed0ba36df667096d071c2894dd052115
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-06 21:09:30 +0200

    Slices with first, niddle or last

--- python-crash-course/bab-4-working-with-lists/4-10-slices.py
+++ python-crash-course/bab-4-working-with-lists/4-10-slices.py
@@ -1,9 +1,15 @@
 #Using one of the progran you wrote in this chapter, add several lines to the end of the program that do the following
 animals = ['cow', 'chicken', 'pig', 'panda', 'monkey']
+print(f"list of the {animals}")
 
-# Print the messsage 
+# Print the messsage, then using slices to print first three item from that program list
 print("The first three items in the list are : ")
-
-#Then using slices to print first three item from that program list
-
 print(animals[0:3])
+
+#Then print the message Three items from the middle of the list and using slices
+print("The first three items in the list are : ")
+print(animals[1:4])
+
+#Then print the message Three items from the last of the list and using slices
+print("The last three items in the list are : ")
+print(animals[2:])
