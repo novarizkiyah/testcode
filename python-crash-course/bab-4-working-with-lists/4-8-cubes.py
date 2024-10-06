9015dea86e8ffc3099cd3bca5ec7d6c203a83f14
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-06 10:43:12 +0200

    Cubes

---
+++ python-crash-course/bab-4-working-with-lists/4-8-cubes.py
@@ -0,0 +1,9 @@
+# thrid power is called cube
+# make a list the first 10 cubes
+
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
+
+# use for loop to print out each cube
+for cube in cubes:
+    print(cube)
