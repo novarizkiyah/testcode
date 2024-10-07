66b4323f92053309c85d4d77969548b38a4e90fa
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-07 11:04:35 +0200

    More loops

---
+++ python-crash-course/bab-4-working-with-lists/4-12-more-loops.py
@@ -0,0 +1,16 @@
+#All version of foods.py in this section haved avoided using for loops when printing, to save space
+
+#Chose a version of foods.py 
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+#write two for loops to print each list of foods
+print("My favorite foods are")
+for my_food in my_foods:
+    print(my_food)
+
+print("My friend's favorite food are ")
+
+for friend_food in friend_foods:
+    print(friend_food)
