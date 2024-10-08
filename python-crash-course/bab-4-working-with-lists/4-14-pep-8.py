28df633d5f0fc693d804e3f636a277fa27f93572
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-08 13:16:03 +0200

    PEP 8

---
+++ python-crash-course/bab-4-working-with-lists/4-14-pep-8.py
@@ -0,0 +1,1 @@
+# Look through the original PEP 8 style guide at https://python.org/dev/peps/pep-0008
---
+++ python-crash-course/bab-4-working-with-lists/4-15-code-review.py
@@ -0,0 +1,3 @@
+#Choose 3 program of this chapter and modify each one to comply PEP 8
+
+# Use four spaced for each indention level.
