67153ba86f7b45d26a12789de3143e73973de9f4
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-10 19:14:46 +0200

    Add checking username

---
+++ python-crash-course/bab-5-if-statement/5-10-checking-username.py
@@ -0,0 +1,13 @@
+# Do the following to create a program that simulates how websites ensure that everyone has a unique username
+
+# 1. Make a list of five or more usernames called current_users
+
+# 2. Make another list of five username called new_users. Make sure ibe ir two of the new usernames are also in the current_users list
+
+# 3. Loop through the new_user list to see if each new username has already been used. 
+# If it has, print a message that the person will need to enter a new username.
+# If a username has not been used, print a message saying that the username is available.
+
+# 4. Make sure your comparison is case insensitive. 
+# If 'John' has been used, 'JOHN' should not be accepted.
+# (To do this, you'll need to make a copy of current_users containing the lowercase versions of all existing users.)
