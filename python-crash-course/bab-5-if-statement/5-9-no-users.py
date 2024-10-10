758149a22034cb3fb554cebb5375faa4217b36b7
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-10 19:03:35 +0200

    No users

---
+++ python-crash-course/bab-5-if-statement/5-9-no-users.py
@@ -0,0 +1,5 @@
+# Add an if test to hello_admin.oy to make sure the list of uders is not empty
+
+# 1. If the list is e,pty. print the message : We need to find some users!
+
+# 2. Remove all of the usernames from your list and make sure the correct message is printed.
