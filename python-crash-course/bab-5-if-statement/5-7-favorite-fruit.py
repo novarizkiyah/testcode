5cb2e66baf9b01df51080effab8770bb6e0fd61b
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-10 18:34:30 +0200

    Add favorite

---
+++ python-crash-course/bab-5-if-statement/5-7-favorite-fruit.py
@@ -0,0 +1,6 @@
+# Make a list of your favorite fruits and then write a series of independent if statements that check for certain fruits in your list
+
+# 1. Make a list of your three favorite fruits and call it favorite_fruits.
+
+# 2. Write five if statement, each should check a certain fruit is in your list.
+# If the fruit in your list, print a statement like : You really like bananas!
