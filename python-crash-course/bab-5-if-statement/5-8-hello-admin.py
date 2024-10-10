4c6dafd1c7268cbdf758c8ca961b4dbe8ccbbdc7
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-10 18:50:54 +0200

    Hello admin

---
+++ python-crash-course/bab-5-if-statement/5-8-hello-admin.py
@@ -0,0 +1,6 @@
+# Make a list of five or more usernames, including the name 'admin'
+# Imagine you are writing the code that will print a greeting to each user after they log in to a web.
+# Loop through the list, and print a greeting to each user
+
+# 1. If the username is 'admin', print a special greeting, such as : Hello admin, would you like to see a status report.
+# 2. Print a generic greeting, such as Hello Jardem thank you for logging in again 
