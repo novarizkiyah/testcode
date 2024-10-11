45dbc9c3f37e7b571743706f0921eab767385ef6
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-11 13:32:17 +0200

    alien colors uding if-if and if-else

--- python-crash-course/bab-5-if-statement/5-4-alien-colors-2.py
+++ python-crash-course/bab-5-if-statement/5-4-alien-colors-2.py
@@ -1,7 +1,19 @@
-# chose a color for an anlien as you did in exercise before and write an if-else chain
+# chose a color for an alien as you did in exercise before and write an if-else chain
 
 # 1. If aliens is green, print statemen that the player just earn 5 points.
 
 # 2. if the alien's color is isn't green, print a statement that the player just earned 10 points
 
 # 3. Write one version of this program that runs the if block and another that runs the else block
+
+alien_colors = ["green", "yellow", "red"]
+
+if "green" in alien_colors:
+    print("You just earn 5 points")
+if "green" not in alien_colors:
+    print("You just earn 10 points")
+
+if "green" in alien_colors:
+    print("You just earn 5 points")
+else:
+    print("You just earn 10 points")
