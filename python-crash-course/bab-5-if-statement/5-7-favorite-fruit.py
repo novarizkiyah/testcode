e8340a209261b96bee75500d47573690752a9918
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-11 16:49:11 +0200

    Favorite fruits

--- python-crash-course/bab-5-if-statement/5-7-favorite-fruit.py
+++ python-crash-course/bab-5-if-statement/5-7-favorite-fruit.py
@@ -4,3 +4,16 @@
 
 # 2. Write five if statement, each should check a certain fruit is in your list.
 # If the fruit in your list, print a statement like : You really like bananas!
+
+favorite_fruits = ["banana", "apple", "kiwi"]
+
+if "banana" in favorite_fruits:
+    print("You are like banana")
+if "mengkudu" in favorite_fruits:
+    print("You are like mengkudu")
+if "apple" in favorite_fruits:
+    print("You're like apple")
+if "kiwi" in favorite_fruits:
+    print("you're like kiwi")
+if "durian" in favorite_fruits:
+    print("you're like durian")
