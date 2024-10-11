b9892758fa4c3ea2079fb6288e05e3ef02e1f6cb
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-11 13:24:06 +0200

    Alien colors in lists

--- python-crash-course/bab-5-if-statement/5-3-alien-colors.py
+++ python-crash-course/bab-5-if-statement/5-3-alien-colors.py
@@ -1,16 +1,17 @@
 #Imagine an alien was just shot down in a game
-#Create a variable name alien_color abd assign it a value of "green", "yellow", "red"
-alien_color = ['green', 'yellow', 'red']
+#Create a variable name alien_colors and assign it a value of "green", "yellow", "red"
+
+alien_colors = ["green", "yellow", "red"]
 
 #Write an if statement to test whether the alien's color is green.
 #If it is, print a message that the player just earn 5 points.
-
-if 'green' in alien_color:
-    print("you earn 5 points")
+if "green" in alien_colors:
+    print("Your earn 5 points")
 
 #Write one version of this program that passes the if test and another that fails
 #(The version that fails will have no output)
-if 'green' in alien_color:
-    print("you earn 5 points")
-if 'black' in alien_color:
-    print("you earn 6 points")
+
+if "yellow" in alien_colors:
+    print("You earn 7 points")
+if "black" in alien_colors:
+    print("You earn 2 points")
