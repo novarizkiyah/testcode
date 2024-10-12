04c857f846fbfe46581e6e77f9617e235af4307d
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-12 20:39:40 +0200

    No users

--- python-crash-course/bab-5-if-statement/5-9-no-users.py
+++ python-crash-course/bab-5-if-statement/5-9-no-users.py
@@ -1,5 +1,14 @@
-# Add an if test to hello_admin.oy to make sure the list of uders is not empty
+# Add an if test to hello_admin.py to make sure the list of users is not empty
 
-# 1. If the list is e,pty. print the message : We need to find some users!
+# 1. If the list is empty. print the message : We need to find some users!
 
 # 2. Remove all of the usernames from your list and make sure the correct message is printed.
+
+usernames = []
+
+if usernames:
+    for username in usernames:
+            print(f"Adding {username}")
+    print("Add admin")
+else:
+    print("We need find some users")
