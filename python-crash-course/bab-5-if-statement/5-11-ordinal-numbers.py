049c736a31916ae7774bac5a3f3299a35249c196
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-12 21:37:02 +0200

    Ordinal number

--- python-crash-course/bab-5-if-statement/5-11-ordinal-numbers.py
+++ python-crash-course/bab-5-if-statement/5-11-ordinal-numbers.py
@@ -6,3 +6,15 @@
 
 # 3. Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number.
 # Your output should read "1st 2nd 3rd 4th 5th 6th 7th  8th 9th" and each result should be on a separate line
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers:
+    if number == 1 :
+        print(f"{number}st")
+    elif number == 2:
+        print(f"{number}nd")
+    elif number == 3:
+        print(f"{number}rd")
+    else:
+        print(f"{number}th")
