c3698d1fcdc486030ca342594afa1bd3d062262b
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-14 21:13:45 +0200

    Favorite number

---
+++ python-crash-course/bab-6-dictionaries/6-2-favorite-numbers.py
@@ -0,0 +1,14 @@
+# Think of five names and use them as keys in your dictionaries
+# Think of a favorite number for each person and stored as value then print the dictionaries
+
+favorite_numbers = {
+    'nova' : 7,
+    'lela' : 11,
+    'mena' : 14,
+    'kuyu' : 9,
+    'meida' : 18
+}
+
+number = favorite_numbers['nova']
+print(favorite_numbers)
+print(f"Nova favorite number is {number}")
