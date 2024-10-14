b2c0cf344ae4975363966ba95d505e743bf336d6
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-14 20:49:46 +0200

    Identity

---
+++ python-crash-course/bab-6-dictionaries/6-1-person.py
@@ -0,0 +1,12 @@
+# Use dictionary to store person you know
+# store the first_name, last_name, age and the city.
+# print each piece of information stored in your dictionaries
+
+identity = {
+    'first name' : 'nova',
+    'last_name' : 'rizkiyah',
+    'age' : 31,
+    'city' : 'rotterdam'
+}
+
+print(identity)
