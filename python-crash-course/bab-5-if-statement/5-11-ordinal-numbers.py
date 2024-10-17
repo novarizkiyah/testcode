f68f0f5c7b3c07b791a6142450dcdb3d7bdcc22c
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-17 14:20:37 +0200

    Ordinal numbers

--- python-crash-course/bab-5-if-statement/5-11-ordinal-numbers.py
+++ python-crash-course/bab-5-if-statement/5-11-ordinal-numbers.py
@@ -10,7 +10,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for number in numbers:
-    if number == 1 :
+    if number == 1:
         print(f"{number}st")
     elif number == 2:
         print(f"{number}nd")
