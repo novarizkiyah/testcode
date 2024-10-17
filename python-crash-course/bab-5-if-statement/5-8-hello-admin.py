e99649d171cb92127ce1c31bc4e3d0c7c75d4ccf
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-17 14:03:48 +0200

    Hello Admin 2

--- python-crash-course/bab-5-if-statement/5-8-hello-admin.py
+++ python-crash-course/bab-5-if-statement/5-8-hello-admin.py
@@ -5,13 +5,10 @@
 # 1. If the username is 'admin', print a special greeting, such as : Hello admin, would you like to see a status report.
 # 2. Print a generic greeting, such as Hello Jardem thank you for logging in again 
 
-
 lists = ["nova", "rizkiyah", "admin", "mulyono", "fufufafa"]
-
-
 
 for list in lists:
     if list == "admin":
         print("Hello Admin, would you like to see a status?")
     else:
-        print(f"Hello, {list.title()} thank you for logging again")
+        print(f"Hello, {list.title()} thank you for loggin again")
