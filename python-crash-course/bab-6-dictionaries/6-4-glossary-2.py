7d4d61e10ffd738c438e4394f683715375358b05
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-18 21:39:30 +0200

    Add questions

---
+++ python-crash-course/bab-6-dictionaries/6-4-glossary-2.py
@@ -0,0 +1,3 @@
+#Now that you know how to loop through a dictionary, clean up the code from Exercise 6-3 (page 102) by replacing your series of print statements with a loop that runs through the dictionary’s keys and values. 
+#When you’re sure that your loop works, add five more Python terms to your glossary. 
+#When you run your program again, these new words and meanings should automatically be included in the output.
