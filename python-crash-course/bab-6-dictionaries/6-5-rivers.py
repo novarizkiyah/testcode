5acd8f9ba0fd0ed2907cebfc8b8807a6030ade11
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-20 14:52:58 +0200

    Add Rivers

--- python-crash-course/bab-6-dictionaries/6-5-rivers.py
+++ python-crash-course/bab-6-dictionaries/6-5-rivers.py
@@ -7,3 +7,17 @@
 
 '''
     
+Rivers_flow_in_you = {
+    "Nile" : "Egypt",
+    "gangga" : "India",
+    "Aare" : "Swizzerland"
+}
+
+for river, country in Rivers_flow_in_you.items():
+    print(f"{river} run through {country}")
+
+for river in Rivers_flow_in_you:
+    print(river)
+
+for country in Rivers_flow_in_you:
+    print(country)
