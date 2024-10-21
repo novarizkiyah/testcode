4f85eac015a7510d7a8126b9a01aad0fbd3886b8
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-21 19:29:37 +0200

    Python

--- python-crash-course/bab-6-dictionaries/6-10-favorite-numbers.py
+++ python-crash-course/bab-6-dictionaries/6-10-favorite-numbers.py
@@ -1,3 +1,12 @@
 # Modify your program from Exercise 6-2 (page 102) so each person can have more than one favorite number. 
 # Then print each person’s name along with their favorite numbers.
 
+favorite_places = {
+    'nova': ['jogja', 'jakarta', 'paris'],
+    'mew': ['solo', 'madiun', 'mekkah'],
+    'rina': ['madinah', 'bern', 'osaka']
+}
+
+# Correct way to iterate over the dictionary
+for name, places in favorite_places.items():  # Use .items() to get both key and value
+    print(name)
--- python-crash-course/bab-6-dictionaries/6-9-favorite-places.py
+++ python-crash-course/bab-6-dictionaries/6-9-favorite-places.py
@@ -2,3 +2,16 @@
 # Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. 
 # To make this exericse a bit more interesting, ask some friends to name a few of their favorite places. 
 # Loop through the dictionary, and print each person’s name and their favorite places.
+
+favorite_places = {
+    'nova': ['jogja', 'jakarta', 'paris'],
+    'mew': ['solo', 'madiun', 'mekkah'],
+    'rina': ['madinah', 'bern', 'osaka']
+}
+
+for name, places in favorite_places.items():
+    print(f"My name is {name.title()} is ")
+    for place in places:
+        print(f"{place}")
+
+
