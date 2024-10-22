5af74ed358f82a92812175715ec1e43553b34bbd
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-22 13:16:49 +0200

    People in list

--- python-crash-course/bab-6-dictionaries/6-7-people.py
+++ python-crash-course/bab-6-dictionaries/6-7-people.py
@@ -1,40 +1,33 @@
 # Start with the program you wrote for Exercise 6-1 (page 98). 
 # Make two new dictionaries representing different people, 
-# and store all three dictionaries in a list called people. 
+# and store all three dictionaries in a list called peoples. 
 # Loop through your list of people. As you loop through the list, 
 # print everything you know about each person.
 
-identities_1 = {
+identity_1 = {
     'first name' : 'nova',
     'last name' : 'rizkiyah',
     'age' : 31,
     'city' : 'rotterdam'
 }
-
-identities_2 = {
-    'first name' : 'wanda',
-    'last name' : 'kyaira',
-    'age' : 2,
-    'city' : 'paris'
+identity_2 = {
+    'first name' : 'yamin',
+    'last name' : 'moe',
+    'age' : 21,
+    'city' : 'sydney'
+}
+identity_3 = {
+    'first name' : 'kala',
+    'last name' : 'mahla',
+    'age' : 11,
+    'city' : 'jakarta'
 }
 
-identities_3 = {
-    'first name' : 'nina',
-    'last name' : 'herna',
-    'age' : 3,
-    'city' : 'london'
-}
-
-peoples = [identities_1, identities_2, identities_3]
-
-for people in peoples:
-    print(people)
+peoples = [identity_1, identity_2, identity_3]
 
 for ppl in peoples:
     print(
-        f"My first name is {ppl['first name'].title()}, ",
-        f"my last name is {ppl['last name'].title()}, ",
-        f"my age is {ppl['age']} years, ", 
-        f"I live in {ppl['city'].title()}"
-        )
-
+        f"Name : {ppl['first name'].title()} {ppl['last name'].title()}"
+        f"\n Age : {ppl['age']} years old"
+        f"\n Live in {ppl['city']}"
+    )
