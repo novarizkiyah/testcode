b7d9e885c5fe2bd0e0abbd43370da2634c426dd6
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-22 11:31:19 +0200

    favoite numbers

--- python-crash-course/bab-6-dictionaries/6-2-favorite-numbers.py
+++ python-crash-course/bab-6-dictionaries/6-2-favorite-numbers.py
@@ -1,17 +1,18 @@
 # Think of five names and use them as keys in your dictionaries
 # Think of a favorite number for each person and stored as value then print the dictionaries
 
+
 favorite_numbers = {
-    'nova' : 7,
-    'lela' : 11,
-    'mena' : 14,
-    'kuyu' : 9,
-    'meida' : 18
+    'nova' : [7, 9, 6],
+    'yasmin' : 4,
+    'rizki' : 2,
+    'giring' : 8,
+    'maya' : 10
 }
 
-number = favorite_numbers['nova']
 print(favorite_numbers)
-print(f"Nova favorite number is {number}")
-
-for k, v in favorite_numbers.items():
-    print(f"{k.title()} favorite number is {v}")
+print(f"Nova's favorite number is {favorite_numbers['nova']}")
+print(f"Yasmin favorite number is {favorite_numbers['yasmin']}")
+print(f"Rizki favorite number is {favorite_numbers['rizki']}")
+print(f"Giring favorite number is {favorite_numbers['giring']}")
+print(f"Maya favorite number is {favorite_numbers['maya']}")
