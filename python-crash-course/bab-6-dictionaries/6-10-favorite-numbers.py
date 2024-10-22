80c719be38d286472748e0a21a07913d245c9ff5
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-22 14:09:13 +0200

    favorite number

--- python-crash-course/bab-6-dictionaries/6-10-favorite-numbers.py
+++ python-crash-course/bab-6-dictionaries/6-10-favorite-numbers.py
@@ -1,14 +1,17 @@
 # Modify your program from Exercise 6-2 (page 102) so each person can have more than one favorite number. 
 # Then print each person’s name along with their favorite numbers.
 
+
 favorite_numbers = {
-    'nova': [1, 7, 6, 4],
-    'mew': [5, 6, 1, 6, 3],
-    'rina': [2, 5]
+    'nova' : [7, 9, 6],
+    'yasmin' : [4, 5, 2],
+    'rizki' : [2, 5],
+    'giring' : [8, 4, 2],
+    'maya' : [10, 1, 8]
 }
 
-# Correct way to iterate over the dictionary
-for name, numbers in favorite_numbers.items(): 
-    print(f"My name is {name.title()} and my favorite number is")
+for name, numbers in favorite_numbers.items():
+    print(f"My name is {name}")
+    print(f"My favorite number is")
     for number in numbers:
-        print(f"\t {number}")
+        print(f"{number}")
