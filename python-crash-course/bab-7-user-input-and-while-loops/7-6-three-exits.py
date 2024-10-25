8053c6a3e0a0a6732825349d7a503ad8cbcd308c
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-25 19:19:53 +0200

    Add questions 6 n 7

---
+++ python-crash-course/bab-7-user-input-and-while-loops/7-6-three-exits.py
@@ -0,0 +1,7 @@
+# Write different version excercise 7-5 that do following at least once:
+
+# use a conditional test in while statement to stop the loop
+
+# use an active variable to control flow how long the loop runs
+
+# use break statement to exit the loop when the user enter a 'quit' value
---
+++ python-crash-course/bab-7-user-input-and-while-loops/7-7-infinity.py
@@ -0,0 +1,2 @@
+# write a loop that never ends, and run it
+#to end the loop, press CTRL+C 
