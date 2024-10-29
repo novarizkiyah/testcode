b08570f2720815f5bc652d81292334b773abfeb8
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-29 16:27:00 +0100

    Additional

--- python-crash-course/bab-7-user-input-and-while-loops/7-6-three-exits.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-6-three-exits.py
@@ -5,3 +5,28 @@
 # use an active variable to control flow how long the loop runs
 
 # use break statement to exit the loop when the user enter a 'quit' value
+
+active = True
+
+while active:
+    user_input = input("How old are you? (please type 'quit' to quit)")
+
+    if user_input.lower() == 'quit':
+        active = False
+        print("Thank you!")
+        break
+
+    try:
+        number = int(user_input)
+        if 0 < number <= 3:
+            print("The ticket is free.")
+        elif 3 < number < 12:
+            print("The ticket is $10.")
+        else:
+            print("The ticket is $15.")
+    except ValueError:
+        print("Please enter a valid age or 'quit' to exit.")
+
+
+
+
--- python-crash-course/bab-7-user-input-and-while-loops/7-7-infinity.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-7-infinity.py
@@ -1,2 +1,7 @@
 # write a loop that never ends, and run it
 #to end the loop, press CTRL+C 
+
+x = 4
+
+while x<=6:
+    print("hello")
