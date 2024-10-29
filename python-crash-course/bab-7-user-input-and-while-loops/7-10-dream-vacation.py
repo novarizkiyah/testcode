ad699ac509e7b42d3432188a4cfae7d8e66ea4eb
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-29 17:29:07 +0100

    Dream Vacation

--- python-crash-course/bab-7-user-input-and-while-loops/7-10-dream-vacation.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-10-dream-vacation.py
@@ -1,3 +1,17 @@
 #Write a program that polls users about their dream vacation. 
-# Write a prompt similar to If you could visit one place in the world, where would you go? 
+# Write a prompt similar to "If you could visit one place in the world, where would you go?" 
 # Include a block of code that prints the results of the poll.
+ 
+responses = []
+
+polling_active = True
+
+while polling_active:
+    response = input("If you could visit one place in the world, where would you go?")
+    if response.lower() == 'quit':
+        break
+    else:
+        responses.append(response)
+
+for response in responses:
+    print(response)
