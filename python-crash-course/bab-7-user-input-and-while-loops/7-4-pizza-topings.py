b256a0e90333f35678ed081020ebb605a01ca075
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-30 11:18:41 +0100

    Using while statement

--- python-crash-course/bab-7-user-input-and-while-loops/7-4-pizza-topings.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-4-pizza-topings.py
@@ -1,10 +1,12 @@
 #Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. 
 # As they enter each topping, print a message saying you’ll add that topping to their pizza.
 
-message = ""
-while message != 'quit':
-    message = input("\nEnter pizza toping ? ")
-    if message != 'quit':
-        print(f"I'll add {message} to your pizza")
 
+
+message = ""
+
+while message != 'quit':
+    message = input("Please enter topping pizza ")
+    if message != 'quit':
+        print(f"I will add {message.title()} to your topping pizza")
 
