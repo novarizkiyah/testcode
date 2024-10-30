4b6d49cc9944cf03d0b2fcd36099b48c02bf9406
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-30 12:25:14 +0100

    Using integer input and using if - elif-else

--- python-crash-course/bab-7-user-input-and-while-loops/7-5-movie-tickets.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-5-movie-tickets.py
@@ -2,14 +2,15 @@
 # If a person is under the age of 3, the ticket is free; 
 # if they are between 3 and 12, the ticket is $10; 
 # and if they are over age 12, the ticket is $15. 
-# Write a loop in which you ask users their age, and then tel them the cost of their movie ticket.
+# Write a loop in which you ask users their age, and then tell them the cost of their movie ticket.
 
-number = int(input("How old are you? "))
+
 
 while True:
-    if 0<number<=3:
-        print("the ticket is free")
-    elif 3 < number < 12:
+    number = int(input("How old are you? "))
+    if 0 < number <= 3:
+        print("The ticket is free")
+    elif 3 < number <= 12:
         print("the ticket is $10")
     else:
         print("the ticket is $15")
