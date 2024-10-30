dac0fe0c57dfe13c27c9962f3fd7cb8c832e9ee7
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-30 13:05:46 +0100

    Using two list and using pop and append method

--- python-crash-course/bab-7-user-input-and-while-loops/7-8-deli.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-8-deli.py
@@ -4,22 +4,13 @@
 # such as I made your tuna sandwich. As each sandwich is made, move it to the list of finished sandwiches. 
 # After all the sandwiches have been made, print a message listing each sandwich that was made.
 
-sandwich_orders = ['sandwich A', 'sandwich B', 'sandwich C']
+sandwich_orders = ['tuna', 'beef', 'lamb']
 finished_sandwiches = []
 
-for sandwich_order in sandwich_orders:
-    print(sandwich_order)
-
 while sandwich_orders:
-    current_sandwich = sandwich_orders.pop()
+    current_sandwiches = sandwich_orders.pop()
+    print(f"I finished the {current_sandwiches}")
+    finished_sandwiches.append(current_sandwiches)
 
-    print(f"I made a {current_sandwich}")
-    finished_sandwiches.append(current_sandwich)
-
-
-print("List my sadwiches:")
 for finished_sandwich in finished_sandwiches:
-    print(finished_sandwich)
-
-
-   
+    print(f"There are {finished_sandwich}")
