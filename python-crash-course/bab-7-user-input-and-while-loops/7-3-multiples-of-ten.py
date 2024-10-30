ea14f001de8282850d29385d0deb486319b3f5b2
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-30 10:35:24 +0100

    Using integer input and using if-else

--- python-crash-course/bab-7-user-input-and-while-loops/7-3-multiples-of-ten.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-3-multiples-of-ten.py
@@ -1,8 +1,8 @@
 #Ask the user for a number, and then report whether the number is a multiple of 10 or not.
 
-number = int(input("Is this number is multiple of 10? "))
+number = int(input("Is the number multiple of 10? "))
 
 if number % 10 == 0 and number != 0:
-    print("The number is multiple of 10")
+    print("the number is multiple of 10")
 else:
-    print(f"The number {number} is not multiple of 10")
+    print(f"the {number} is not multiple of 10")
