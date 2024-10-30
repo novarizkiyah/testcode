ffe8c1cb4a5b1abdc294629dedba24e19c467f81
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-10-30 10:05:44 +0100

    Using input method

--- python-crash-course/bab-7-user-input-and-while-loops/7-1-rental-car.py
+++ python-crash-course/bab-7-user-input-and-while-loops/7-1-rental-car.py
@@ -1,5 +1,5 @@
 #Write a program that asks the user what kind of rental car they would like. 
 # Print a message about that car, such as “Let me see if I can find you a Subaru”.
 
-message = input("What kind of car do you want to rental?")
-print("let me see if I can find you a " + message.title())
+message = input("What kind of car do you want to rent?")
+print(f"Let me see if I can find you a {message.title()}")
