03a81304c8ac07388878a2f391867c60aa9116ea
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-01 14:04:45 +0100

    Add favorite book

--- python-crash-course/bab-8-functions/8-2-favorite-book.py
+++ python-crash-course/bab-8-functions/8-2-favorite-book.py
@@ -1,3 +1,8 @@
 #Write a function called favorite_book() that accepts one parameter, title. 
 # The function should print a message, such as One of my favorite books is Alice in Wonderland. 
 # Call the function, making sure to include a book title as an argument in the function call.
+
+def favorite_book(book_title):
+    print(f"One of my favorite book is {book_title}")
+
+favorite_book("Alice in Wonderland")
