e63ffcf6a431db2670c81cbda408cee60137952b
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-01 13:59:09 +0100

    Add function

--- python-crash-course/bab-8-functions/8-1-message.py
+++ python-crash-course/bab-8-functions/8-1-message.py
@@ -2,3 +2,7 @@
 # telling everyone what you are learning about in this chapter. 
 # Call the function, and make sure the message displays correctly.
 
+def display_message():
+    print("I'm learning function")
+
+display_message()
