03c00c97601f8a65f51e3049d6fc57bbf0e5072d
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-02 20:39:18 +0100

    Add Size

--- python-crash-course/bab-8-functions/8-4-large-shirts.py
+++ python-crash-course/bab-8-functions/8-4-large-shirts.py
@@ -1,3 +1,10 @@
 #Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. 
 # Make a large shirt and a medium shirt with the default message, 
 # and a shirt of any size with a different message.
+
+def make_shirt(size, sentences):
+    print(f"I love Python, my size is {size} so {sentences}")
+
+make_shirt("S", "cute")
+make_shirt("L", "large")
+make_shirt("M", "medium")
