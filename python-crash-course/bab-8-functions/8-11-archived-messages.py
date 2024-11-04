37e7b0474c63c9995c2e48320fa7d6e1eb997aa6
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-04 12:02:21 +0100

    Add list

--- python-crash-course/bab-8-functions/8-11-archived-messages.py
+++ python-crash-course/bab-8-functions/8-11-archived-messages.py
@@ -1,6 +1,21 @@
 #Start with your work from Exercise 8-10. 
-# Call the function make_great() with a copy of the list of magicians’ names. 
-# Because the original list will be unchanged, 
-# return the new list and store it in a separate list. 
-# Call show_magicians() with each list to show that you have one list of the original names and 
-# one list with the Great added to each magician’s name.
+# Call the function send_messages() with a copy of the list of the messages
+# After calling the function,
+# print both of your lists to show that the original list has retained its messages
+
+def print_messages(messages, sent_messages):
+    while messages:
+        current_message = messages.pop()
+        print(f"Current messages is {current_message}")
+        sent_messages.append(current_message)
+def show_messages(sent_messages):
+    print("This is the complete sent messages :")
+    for message in sent_messages:
+        print(message)
+messages = ['Hi Epriibodiihh',
+              'Hi Prabowoooooo',
+               'Hi Pusaosadakdnuvo']
+sent_messages = []
+
+print_messages(messages[:], sent_messages)
+show_messages(sent_messages)
