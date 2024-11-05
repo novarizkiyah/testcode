ff18a44a4089c10bf5587db03156080acaacaeb2
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-05 09:50:23 +0100

    Sandwiches

--- python-crash-course/bab-8-functions/8-12-sandwiches.py
+++ python-crash-course/bab-8-functions/8-12-sandwiches.py
@@ -2,3 +2,12 @@
 # The function should have one parameter that collects as many items as the function call provides, 
 # and it should print a summary of the sandiwch that is being ordered. 
 # Call the function three tiems, using a different number of arguments each time.
+
+def sandwiches(*items):
+    print("List of items")
+    for item in items:
+        print(f"- {item}")
+
+sandwiches('item 1', 'item 3', 'item 2')
+sandwiches('item 4', 'item 2')
+sandwiches('item 1', 'item 4')
