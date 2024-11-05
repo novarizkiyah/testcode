f89868a0e1c02e904571112c2d553661a960e55c
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-05 09:52:22 +0100

    Add questions user profile

---
+++ python-crash-course/bab-8-functions/8-13-user-profile.py
@@ -0,0 +1,3 @@
+# Start with a copy of user_profile.py from page 148
+# Build a profile of yourself by calling build_profile(),
+# using your first and last names and three other key-value pairs that describe you
