2307e1c701f0781cd7e0aaacde790157a174694e
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-05 10:47:45 +0100

    Add User profile

--- python-crash-course/bab-8-functions/8-13-user-profile.py
+++ python-crash-course/bab-8-functions/8-13-user-profile.py
@@ -1,3 +1,15 @@
 # Start with a copy of user_profile.py from page 148
 # Build a profile of yourself by calling build_profile(),
 # using your first and last names and three other key-value pairs that describe you
+
+def build_profile(first, last, **user_info):
+    ''' Build a dictionary containing everything about the user'''
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('nova', 'rizkiyah',
+                             location = 'rotterdam',
+                             field = 'physics',
+                             favorite_food = 'martabak')
+print(user_profile)
