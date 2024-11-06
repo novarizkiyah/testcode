1e7798bc728619b4c30dae2d67476097bc961f96
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-06 11:08:22 +0100

    Adding parameter and argument

--- python-crash-course/bab-8-functions/8-2-favorite-book.py
+++ python-crash-course/bab-8-functions/8-2-favorite-book.py
@@ -2,7 +2,8 @@
 # The function should print a message, such as One of my favorite books is Alice in Wonderland. 
 # Call the function, making sure to include a book title as an argument in the function call.
 
-def favorite_book(book_title):
-    print(f"One of my favorite book is {book_title}")
+def favorite_book(title):
+    print(f"One of my favorite book is {title}")
 
 favorite_book("Alice in Wonderland")
+favorite_book("Python Crash Course")
