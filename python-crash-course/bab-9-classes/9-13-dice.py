22addff63f0bd09c9a2f85e2390b620b782a04e1
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-10 22:00:03 +0100

    Add questions again

---
+++ python-crash-course/bab-9-classes/9-13-dice.py
@@ -0,0 +1,9 @@
+#Start with Exercise 6-4 (page 108), 
+# where you used a standard dictionary to represent a glossary. 
+# Rewrite the program using the OrderedDict class 
+# and make sure the order of the output matches the order 
+# in which key-value pairs were added to the dictionary.
+
+#Note: In Python 3.6, dictionaries store keys in order. 
+# However, this is not guaranteed to work in all versions of Python, 
+# so you should still use an OrderedDict when you need key-value pairs to be stored in a particular order.
