e00d048cf103669e115ec22ea7d60e6aecb1387d
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-12 15:57:31 +0100

    Using arbitrary

--- python-crash-course/bab-8-functions/8-14-cars.py
+++ python-crash-course/bab-8-functions/8-14-cars.py
@@ -3,16 +3,14 @@
 # It should then accept an arbitrary number of keyword arguments. 
 # Call the function with the required information and two other name-value pairs, 
 # such as a color or an optional feature. Your function should work for a call like this one:
-
 # car = make_car('subaru', 'outback', color='blue', tow_package=True)
-
 # Print the dictionary that’s returned to make sure all the information was stored correctly.
 
-def build_car(manufactur, model, **user_info):
-    user_info['manufacturs'] = manufactur
-    user_info['models'] = model
-    return user_info
+def make_car(manufacture, model, **profile_info):
+    profile_info['manufacture'] = manufacture
+    profile_info['model'] = model
+    return profile_info
 
-car_profile = build_car('subaru', 'outback', 
-                        color = 'blue', tow_package = True)
-print(car_profile)
+car = make_car('subaru', 'outback',
+               color = 'blue', tow_package=True) 
+print(car)
