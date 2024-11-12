72ea83ef2576e23b1ec27c6dcc831d21bffd673e
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-12 15:51:23 +0100

    Using arbitrary keyword arguments

--- python-crash-course/bab-8-functions/8-13-user-profile.py
+++ python-crash-course/bab-8-functions/8-13-user-profile.py
@@ -3,13 +3,12 @@
 # using your first and last names and three other key-value pairs that describe you
 
 def build_profile(first, last, **user_info):
-    ''' Build a dictionary containing everything about the user'''
     user_info['first_name'] = first
     user_info['last_name'] = last
     return user_info
 
-user_profile = build_profile('nova', 'rizkiyah',
-                             location = 'rotterdam',
-                             field = 'physics',
-                             favorite_food = 'martabak')
-print(user_profile)
+user_info = build_profile('nova', 'rizkiyah',
+                          location = 'rotterdam', 
+                          hobby = 'reading a book')
+
+print(user_info)
