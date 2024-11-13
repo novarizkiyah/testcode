e411af6f6b962a2f87e0f6a298da1bd0f149a0a8
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-13 20:30:46 +0100

    Add and modified

--- python-crash-course/bab-8-functions/8-17-styling-functions.py
+++ python-crash-course/bab-8-functions/8-17-styling-functions.py
@@ -1,3 +1,2 @@
 # Choose ant three programs you wrote for this chapter
 # and make sure they follow the styling guidelines describes in this section
-
--- python-crash-course/bab-8-functions/printing_models.py
+++ python-crash-course/bab-8-functions/printing_models.py
@@ -1,20 +1,7 @@
 #Put the functions for the example printing_models.py in a separate file called printing_functions.py. 
 # Write an import statement at the top of printing_models.py,
 #  and modify the file to use the imported functions.
-'''
-from printing_functions import print_models, show_complete_models
 
-
-# Create lists of designs that need to be printed
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
-
-# Simulate printing each design
-print_models(unprinted_designs, completed_models)
-
-# Display all completed models
-show_complete_models(completed_models)
-'''
 
 from printing_function import print_models, show_complete_models
 
