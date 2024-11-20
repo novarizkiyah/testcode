cd9ee872ed1571cfa962d205d6e73cb88234886c
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-20 21:19:46 +0100

    Add class

--- python-crash-course/bab-9-classes/9-1-restaurant.py
+++ python-crash-course/bab-9-classes/9-1-restaurant.py
@@ -6,3 +6,13 @@
 
 #Make an instance called restaurant from your class. 
 # Print the two attributes individually, and then call both methods.
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type) :
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} is name of the restaurant")
+        print(f"{self.cuisine_type} is type of cuisine")
+    def open_restaurant(self):
+        print(f"the {self.restaurant_name} is open")
