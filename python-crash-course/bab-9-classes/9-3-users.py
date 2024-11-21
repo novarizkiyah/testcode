b2036017381aede2a6c58b9f86e35506b60bf7cf
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-21 20:56:30 +0100

    Add user

--- python-crash-course/bab-9-classes/9-3-users.py
+++ python-crash-course/bab-9-classes/9-3-users.py
@@ -4,3 +4,22 @@
 # Make another method called greet_user() that prints a personalized greeting to the user.
 
 #Create several instances representing different users, and call both methods for each user.
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    def describe_user(self):
+        print(f"First Name : {self.first_name}")
+        print(f"Last Name : {self.last_name}")
+    def greet_user(self):
+        print(f"Hello {self.first_name} {self.last_name}, welcome in Tokyo!")
+    
+user = User("Nova", "Rizkiyah")
+user2 = User("Aya", "Didut")
+
+user.describe_user()
+user2.describe_user()
+user.greet_user()
+user2.greet_user()
+
