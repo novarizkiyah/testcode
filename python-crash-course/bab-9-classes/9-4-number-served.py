ee7685d453b4accd51d0edf779fb8f2e836d5342
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-23 21:36:51 +0100

    Modifying attribute

--- python-crash-course/bab-9-classes/9-4-number-served.py
+++ python-crash-course/bab-9-classes/9-4-number-served.py
@@ -12,4 +12,35 @@
 # Call this method with any number you like that could represent how many customers were served in, 
 # say, a day of business.
 
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type) :
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} is name of the restaurant")
+        print(f"{self.cuisine_type} is type of cuisine")
+    def open_restaurant(self):
+        print(f"the {self.restaurant_name} is open")
+    def set_number_served(self, number):
+        self.number_served = number
+    def increment_number_served(self, number):
+        """Method to increment the number of customers served."""
+        self.number_served += number
 
+restaurant = Restaurant("Praboemoelih", "Traditional Indonesia")
+
+# Print the number of customers the restaurant has served
+print(f"Customers served initially: {restaurant.number_served}")
+
+# Change the number of customers served and print again
+restaurant.number_served = 50
+print(f"Customers served after change: {restaurant.number_served}")
+
+# Set the number of customers served using the method
+restaurant.set_number_served(100)
+print(f"Customers served after setting new number: {restaurant.number_served}")
+
+# Increment the number of customers served using the method
+restaurant.increment_number_served(30)
+print(f"Customers served after increment: {restaurant.number_served}")
