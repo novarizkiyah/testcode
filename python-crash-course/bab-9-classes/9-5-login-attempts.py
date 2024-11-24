3daeb30a46daf1df59dcb845b938cab6ce9fb770
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-24 21:30:06 +0100

    login attempts

--- python-crash-course/bab-9-classes/9-5-login-attempts.py
+++ python-crash-course/bab-9-classes/9-5-login-attempts.py
@@ -1,8 +1,30 @@
 #Add an attribute called login_attempts to your User class from Exercise 9-3 (page 162). 
-# Write amehtod called increment_login_attempts() that increments the value of login_attempts by 1. 
+# Write another method called increment_login_attempts() that increments the value of login_attempts by 1. 
 # Write another method called reset_login_attempts() that resets the value of login_attempts to 0.
 
 #Make an instance of the User class and call increment_login_attempts() several times. 
 # Print the value of login_attempts to make sure it was incremented properly, 
 # and then call reset_login_attempts(). Print login_attempts again to make sure it was reset to 0.
 
+class User:
+    def __init__(self, first_name, last_name, age, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.login_attempts = 0
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+    
+user = User("Nova", "Rizkiyah", 31, "n@gmail.com")
+
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+print(f"Login attempts after incrementing: {user.login_attempts}")
+
+user.reset_login_attempts()
+print(f"Login attempts after resetting: {user.login_attempts}")
