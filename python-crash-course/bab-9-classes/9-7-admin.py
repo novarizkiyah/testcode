56b6304216eae01e4be50f775dada779c843aa1e
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-26 21:46:13 +0100

    Add privileges

--- python-crash-course/bab-9-classes/9-7-admin.py
+++ python-crash-course/bab-9-classes/9-7-admin.py
@@ -2,7 +2,7 @@
 # Write a class called Admin that inherits from the User class you wrote in Exercise 9-3 (page 162) 
 # or Exercise 9-5 (page 167). 
 # Add an attribute, privileges, that stores a list of strings like "can add post", 
-# "can delete post", "can ban user", and so on. WRite a method called show_privileges() 
+# "can delete post", "can ban user", and so on. Write a method called show_privileges() 
 # that lists the administrator’s set of privileges. Create an instance of Admin, and call your method.
 
 
@@ -16,6 +16,22 @@ class User:
     def greet_user(self):
         print(f"Hello {self.first_name} {self.last_name}, Good Morning!")
 
-user = User("Nova", "Rizkiyah", "novalailatulrizkiyah@gmail.com")
-user.describe_user()
-user.greet_user()
+#user = User("Nova", "Rizkiyah", "novalailatulrizkiyah@gmail.com")
+#user.describe_user()
+#user.greet_user()
+
+class Admin(User):
+    def __init__(self, first_name, last_name, email):
+        super().__init__(first_name, last_name, email)
+        self.privileges = []
+
+    def show_privileges(self):
+        print("Privileges : ")
+        for privilege in self.privileges:
+            print(privilege)
+
+admin = Admin("Nova", "Rizkiyah", "novalailatulrizkiyah@gmail.com")
+admin.describe_user()
+
+admin.privileges = ["can add post", "can delete post", "can ban post"]
+admin.show_privileges()
