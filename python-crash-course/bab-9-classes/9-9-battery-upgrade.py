368cd55bcd4399a2981b81e1a8518b113a6bca48
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-11-28 11:44:37 +0100

    Add battery up

--- python-crash-course/bab-9-classes/9-9-battery-upgrade.py
+++ python-crash-course/bab-9-classes/9-9-battery-upgrade.py
@@ -4,3 +4,36 @@
 # Make an electric car with a default battery size, call get_range() once, 
 # and then call get_range() a second time after upgrading the battery. 
 # You should see an increase in the car’s range.
+
+class Car:
+    '''Simple attempt to represent a car'''
+    def __init__(self, make, model, year) :
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    
+    def get_describe_name(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name
+    def read_odometer(self):
+        '''Print a statement showing the car's mileage'''
+        print(f"This car has {self.odometer_reading} miles on it")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer")
+    def increment_odometer(self, miles):
+        '''Add the given amount to the odometer reading'''
+        self.odometer_reading += miles
+
+class ElecticCar(Car):
+    '''Represent aspect of a car, specific to electric vehicles'''
+    def __init__(self, make, model, year):
+        '''Initialize attributes of the parent class'''
+        super().__init__(make, model, year)
+
+my_leaf = ElecticCar('Nissan', 'leaf', 2024)
+print(my_leaf.get_describe_name())
