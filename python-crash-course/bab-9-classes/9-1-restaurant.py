7610fc4b72bd0268c34b198a2e1e80e7a9ee90fb
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-09 12:34:05 +0100

    creating and using a class

--- python-crash-course/bab-9-classes/9-1-restaurant.py
+++ python-crash-course/bab-9-classes/9-1-restaurant.py
@@ -1,23 +1,23 @@
-#Make a class called Restaurant. 
+# Make a class called Restaurant. 
 # The __init__() method for Restaurant should store two attributes: 
 # a restaurant_name and a cuisine_type. Make a method called describe_restaurant() 
 # that prints these two pieces of information, and a method called open_restaurant() 
 # that prints a message indicating that the restaurant is open.
 
-#Make an instance called restaurant from your class. 
+# Make an instance called restaurant from your class. 
 # Print the two attributes individually, and then call both methods.
 
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type) :
+    def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
     def describe_restaurant(self):
-        print(f"{self.restaurant_name} is name of the restaurant")
-        print(f"{self.cuisine_type} is type of cuisine")
+        print(f"Name : {self.restaurant_name}")
+        print(f"Type : {self.cuisine_type}")
     def open_restaurant(self):
-        print(f"the {self.restaurant_name} is open")\
-        
-restaurant = Restaurant("Praboemoelih", "Traditional Indonesia")
+        print(f"This {self.restaurant_name} is open")
+
+restaurant = Restaurant("Praboemoelih", "Indonesian cuisine")
 print(restaurant.restaurant_name)
 print(restaurant.cuisine_type)
 
