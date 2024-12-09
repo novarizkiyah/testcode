f401440895e28ace247cc9551ff84f49faa319ce
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-09 13:37:11 +0100

    Creating multiple instances and calling methods

--- python-crash-course/bab-9-classes/9-3-users.py
+++ python-crash-course/bab-9-classes/9-3-users.py
@@ -1,29 +1,34 @@
-#Make a class called User. Create two attributes called first_name and last_name, 
+# Make a class called User. Create two attributes called first_name and last_name, 
 # and then create several other attributes that are typically stored in a user profile. 
 # Make a method called describe_user() that prints a summary of the user’s information. 
 # Make another method called greet_user() that prints a personalized greeting to the user.
 
-#Create several instances representing different users, and call both methods for each user.
+# Create several instances representing different users, and call both methods for each user.
 
 class User:
-    def __init__(self, first_name, last_name, age, email):
+    def __init__(self, first_name, last_name, email, passwords):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
         self.email = email
+        self.passwords = passwords
     def describe_user(self):
         print(f"First Name : {self.first_name}")
-        print(f"Last Name : {self.last_name}")
-        print(f"Age : {self.age}")
+        print(f"Last name : {self.last_name}")
         print(f"Email : {self.email}")
+        print(f"Passwords : {self.passwords}")
     def greet_user(self):
-        print(f"Hello {self.first_name} {self.last_name}, welcome in Tokyo!")
-    
-user = User("Nova", "Rizkiyah", 31, "n@gmail.com")
-user2 = User("Aya", "Didut", 6, "aya@gmail.com")
+        print(f"Hallo, {self.first_name} {self.last_name} Welcome in this site! Have a nice day!")
 
-user.describe_user()
+user1 = User("Nova", "Rizkiyah", "n@gmail.com", "twauwag")
+user2 = User("Ratmi", "Mawar", "t@gmail.com", "jdahjadjahd")
+user3 = User("Melati", "Pelangi", "y@gmail.com", "sasanjaa")
+
+user1.describe_user()
+user1.greet_user()
+
 user2.describe_user()
-user.greet_user()
 user2.greet_user()
+
+user3.describe_user()
+user3.greet_user()
 
