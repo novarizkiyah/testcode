e64364e22e56caca633013706a6135f91c4a6e87
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-09 12:52:40 +0100

    Creating Multiple Instances

--- python-crash-course/bab-9-classes/9-2-three-restaurants.py
+++ python-crash-course/bab-9-classes/9-2-three-restaurants.py
@@ -1,4 +1,4 @@
-#Start with your class from Exercise 9-1. 
+# Start with your class from Exercise 9-1. 
 # Create three different instances from the class, 
 # and call describe_restaurant() for each instance.
 
@@ -7,14 +7,14 @@ class Restaurant:
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
     def describe_restaurant(self):
-        print(f"Name Restaurant : {self.restaurant_name}")
-        print(f"Type cuisine : {self.cuisine_type}")
+        print(f"Name : {self.restaurant_name}")
+        print(f"Type : {self.cuisine_type}")
     def open_restaurant(self):
-        print(f"My {self.restaurant_name} is open")
+        print(f"This {self.restaurant_name} is open")
 
-restaurant = Restaurant("Praboemoelih", "Traditional Indonesia")
-restaurant2 = Restaurant("Indian culinary", "Indian")
-restaurant3 = Restaurant("Malaysian food", "Malaysian")
+restaurant = Restaurant("Praboemoelih", "Indonesian cuisine")
+restaurant2 = Restaurant("Mei Mei Restaurant", "Vietnamese cuisine")
+restaurant3 = Restaurant("Waroeng Gareng", "Suriname cuisine")
 
 restaurant.describe_restaurant()
 restaurant2.describe_restaurant()
