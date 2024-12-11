306cffd72da5a0f2bf1673e46c1ac3f9f51dd29b
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-11 21:35:43 +0100

    Add

--- python-crash-course/bab-9-classes/9-7-admin.py
+++ python-crash-course/bab-9-classes/9-7-admin.py
@@ -1,37 +1,43 @@
-#An administrator is a special kind of user. 
-# Write a class called Admin that inherits from the User class you wrote in Exercise 9-3 (page 162) 
-# or Exercise 9-5 (page 167). 
+# An administrator is a special kind of user. 
+# Write a class called Admin that inherits from the User class you wrote in Exercise 9-5 (page 167). 
 # Add an attribute, privileges, that stores a list of strings like "can add post", 
 # "can delete post", "can ban user", and so on. Write a method called show_privileges() 
 # that lists the administrator’s set of privileges. Create an instance of Admin, and call your method.
 
-
 class User:
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, first_name, last_name, email, passwords):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.passwords = passwords
+        self.login_attempts = 0
     def describe_user(self):
-        print(f"My Data : \nName : {self.first_name} {self.last_name}\nEmail : {self.email}")
+        print(f"First Name : {self.first_name}")
+        print(f"Last name : {self.last_name}")
+        print(f"Email : {self.email}")
+        print(f"Passwords : {self.passwords}")
     def greet_user(self):
-        print(f"Hello {self.first_name} {self.last_name}, Good Morning!")
-
-#user = User("Nova", "Rizkiyah", "novalailatulrizkiyah@gmail.com")
-#user.describe_user()
-#user.greet_user()
-
+        print(f"Hallo, {self.first_name} {self.last_name} Welcome in this site! Have a nice day!")
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 class Admin(User):
-    def __init__(self, first_name, last_name, email):
-        super().__init__(first_name, last_name, email)
-        self.privileges = []
+    
 
-    def show_privileges(self):
-        print("Privileges : ")
-        for privilege in self.privileges:
-            print(privilege)
+user = User("Nova", "Rizkiyah", "n@gmail.com", "twauwag")
+user.describe_user()
+user.greet_user()
 
-admin = Admin("Nova", "Rizkiyah", "novalailatulrizkiyah@gmail.com")
-admin.describe_user()
+#Make an instance of the User class and call increment_login_attempts() several times. 
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+print(f"Print the value of login : {user.login_attempts}")
 
-admin.privileges = ["can add post", "can delete post", "can ban post"]
-admin.show_privileges()
+user.reset_login_attempts()
+print(f"Print value after reset : {user.login_attempts}")
+
