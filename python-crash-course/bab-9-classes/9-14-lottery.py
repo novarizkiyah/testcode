c6bc0a06ffb8089944b7639d4aa00a1f3dc11c3a
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-15 18:28:26 +0100

    Add lottery

--- python-crash-course/bab-9-classes/9-14-lottery.py
+++ python-crash-course/bab-9-classes/9-14-lottery.py
@@ -2,3 +2,18 @@
 # Randomly select 4 numbers or letters from the list 
 # and print a message saying that any ticket matching these 4 numbers 
 # or letters wins a prize
+
+from random import choice
+
+num_let = [1,2,3,4,5,6,7,8,9,10,'z', 'y','x','v','w']
+winning_ticket = []
+print("Who is the winner?")
+
+while len(winning_ticket) < 4:
+    pulled_item = choice(num_let)
+
+    if pulled_item not in winning_ticket:
+        print(f"We pulled : {pulled_item}" )
+        winning_ticket.append(pulled_item)
+
+print(f"The final winner is {winning_ticket}")
