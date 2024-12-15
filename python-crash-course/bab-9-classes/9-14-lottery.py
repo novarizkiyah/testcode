b88cbe975005e2fed1d29fa127c7f213ebbefe14
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-15 15:49:13 +0100

    add questions

--- python-crash-course/bab-9-classes/9-14-lottery.py
+++ python-crash-course/bab-9-classes/9-14-lottery.py
@@ -1,1 +1,4 @@
-#
+# Make a list or tuple containing a series of 10 numbers and 5 letters.
+# Randomly select 4 numbers or letters from the list 
+# and print a message saying that any ticket matching these 4 numbers 
+# or letters wins a prize
