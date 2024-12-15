b81afc3b2bb96ef91bd1b56be33606a18c8cccc7
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-15 14:56:40 +0100

    Add questions

---
+++ python-crash-course/bab-9-classes/9-16-python-module-of-the-week.py
@@ -0,0 +1,3 @@
+# One excellent resource for exploring the Python standard library is a site call 
+# Python Module of the Week. Go to https://pymotw.com and look at the table of contents.
+# Find a module that looks interesting to you and read about it, perhaps starting with the random module.
