7fe7e73b4c0bacb123b8c0327762cb387b06afdd
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-15 14:52:43 +0100

    Change

--- python-crash-course/bab-9-classes/9-10-imported-restaurant.py
+++ python-crash-course/bab-9-classes/9-10-imported-restaurant.py
@@ -1,4 +1,4 @@
-#Using your latest Restaurant class, store it in a module. 
+# Using your latest Restaurant class, store it in a module. 
 # Make a separate file that imports Restaurant. 
 # Make a Restaurant instance, and call one of Restaurant’s methods 
 # to show that the import statement is working properly.
