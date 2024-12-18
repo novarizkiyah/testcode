885dd5d1a9ca730519f591353f2138aac2af94c7
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-18 09:20:08 +0100

    Add questions

---
+++ python-crash-course/bab-10-files-and-exceptions/10-3-simpler-code.py
@@ -0,0 +1,8 @@
+# The program file_reader.py in this section uses a temporary variable, lines, 
+# to show how splitlines() works. You can skip the temporary variable 
+# and loop directly over the list that splitlines() returns:
+'''
+for line in contents.splitlines():
+'''
+# Remove the temporary variable from each of the programs in this section, 
+# to make them more concise.
