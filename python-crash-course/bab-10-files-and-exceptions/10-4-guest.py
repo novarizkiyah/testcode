9f583b82c9c768aba98e5996bf418d7ff509d923
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-18 09:21:13 +0100

    Add question chapter 10

---
+++ python-crash-course/bab-10-files-and-exceptions/10-4-guest.py
@@ -0,0 +1,2 @@
+# Write a program that prompts the user for their name. 
+# When they respond, write their name to a file called guest.txt.
---
+++ python-crash-course/bab-10-files-and-exceptions/10-5-guest-book.py
@@ -0,0 +1,4 @@
+# Write a while loop that prompts users for their name. 
+# Collect all the names that are entered, and then write these names 
+# to a file called guest_book.txt. 
+# Make sure each entry appears on a new line in the file.
