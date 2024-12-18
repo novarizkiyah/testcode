04a8ae9916a3aa82fb040aad45ee36726d227b45
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-18 09:57:21 +0100

    Add questions

---
+++ python-crash-course/bab-10-files-and-exceptions/10-11-favorite-number.py
@@ -0,0 +1,4 @@
+# Write a program that prompts for the user's favorite number. 
+# Use json.dumps() to store this number in a file. 
+# Write a separate program that reads in this value and prints the message, 
+# "I know your favorite number! It's _____."
---
+++ python-crash-course/bab-10-files-and-exceptions/10-12-favorite-number-remembered.py
@@ -0,0 +1,4 @@
+# Combine the two programs from Exercise 10-11 into one file. 
+# If the number is already stored, report the favorite number to the user. 
+# If not, prompt for the user's favorite number and store it in a file. 
+# Run the program twice to see that it works.
