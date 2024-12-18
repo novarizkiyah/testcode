b1880135193140ecb0483f9cabd170ef4d6f19ad
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-18 09:59:00 +0100

    Add questions

---
+++ python-crash-course/bab-10-files-and-exceptions/10-13-user-dictionary.py
@@ -0,0 +1,6 @@
+# The remember_me.py example only stores one piece of information, the username. 
+# Expand this example by asking for two more pieces of information about the user, 
+# then store all the information you collect in a dictionary. 
+# Write this dictionary to a file using json.dumps(), 
+# and read it back in using json.loads(). 
+# Print a summary showing exactly what your program remembers about the user.
---
+++ python-crash-course/bab-10-files-and-exceptions/10-14-verify-user.py
@@ -0,0 +1,9 @@
+# The final listing for remember_me.py assumes 
+# either that the user has already entered their username 
+# or that the program is running for the first time. 
+# We should modify it in case the current user is not the person 
+# who last used the program.
+
+# Before printing a welcome back message in greet_user(), 
+# ask the user if this is the correct username. 
+# If it's not, call get_new_username() to get the correct username.
