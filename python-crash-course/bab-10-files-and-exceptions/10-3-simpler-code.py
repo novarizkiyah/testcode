61b538a1604640b21db051202dd7e68c195e0eb4
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-21 18:54:00 +0100

    Guest

--- python-crash-course/bab-10-files-and-exceptions/10-3-simpler-code.py
+++ python-crash-course/bab-10-files-and-exceptions/10-3-simpler-code.py
@@ -7,3 +7,28 @@ for line in contents.splitlines():
 # Remove the temporary variable from each of the programs in this section, 
 # to make them more concise.
 
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+#lines = contents.splitlines()
+#for line in lines:
+#    print(line)
+
+for line in contents.splitlines():
+    print(line)
+
+
+path = Path('pi_million_digits.txt')
+contents = path.read_text()
+
+pi_string = ''
+#lines = contents.splitlines()
+#for line in lines:
+#    pi_string += line.lstrip()
+for line in contents.splitlines():
+    pi_string += line.lstrip()
+print(f"{pi_string[:53]}...")
+print(len(pi_string))
+
--- python-crash-course/bab-10-files-and-exceptions/10-4-guest.py
+++ python-crash-course/bab-10-files-and-exceptions/10-4-guest.py
@@ -1,2 +1,10 @@
 # Write a program that prompts the user for their name. 
 # When they respond, write their name to a file called guest.txt.
+
+from pathlib import Path
+
+
+
+content = input("What is your name? ")
+path = Path('guest.txt')
+path.write_text(content)
