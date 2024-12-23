5f6bf4311e36ea7e0f6905139e249c869a9e014e
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-23 12:02:59 +0100

    Add try except

--- python-crash-course/bab-10-files-and-exceptions/10-8-cats-and-dog.py
+++ python-crash-course/bab-10-files-and-exceptions/10-8-cats-and-dog.py
@@ -7,3 +7,40 @@
 # and print a friendly message if a file is missing. 
 # Move one of the files to a different location on your system, 
 # and make sure the code in the except block executes properly.
+
+from pathlib import Path
+
+contents = ['persian cat', 'american shorthair', 'burmesse cat']
+path = Path("cats.txt")
+file_string = ''
+for content in contents:
+    file_string += f"{content}\n"
+path.write_text(file_string)
+
+content = path.read_text()
+lines = content.splitlines()
+print("Name of cats:")
+for line in lines:
+    print(f"-{line}")
+contents2 = ['siberian husky', 'poodle', 'chihuahua']
+path2 = Path("dogs.txt")
+file_string2 = ''
+for content2 in contents2:
+    file_string2 += f"{content2}\n"
+path2.write_text(file_string2)
+content2 = path.read_text()
+lines2 = content2.splitlines()
+print("Name of dogs:")
+for line2 in lines2:
+    print(f"-{line2}")
+
+filenames = ['cats.txt', 'dogs.txt']
+for filename in filenames:
+    print(f"Reading file : {filename}")
+    path = Path(filename)
+    try:
+        content = path.read_text()
+    except FileNotFoundError:
+        print("The file is missing")
+    else:
+        print(content)
