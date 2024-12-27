c1225cb551989fc541bf5206d052a428665ef030
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-27 15:26:30 +0100

    Add guest

--- python-crash-course/bab-10-files-and-exceptions/10-4-guest.py
+++ python-crash-course/bab-10-files-and-exceptions/10-4-guest.py
@@ -3,6 +3,6 @@
 
 from pathlib import Path
 
-content = input("What is your name? ")
-path = Path('guest.txt')
-path.write_text(content)
+contents = input("What is your name? ")
+path = Path("guest.txt")
+path.write_text(contents)
--- python-crash-course/bab-10-files-and-exceptions/i_like_code.py
+++ python-crash-course/bab-10-files-and-exceptions/i_like_code.py
@@ -23,3 +23,44 @@ lines2 = content2.splitlines()
 print("Name of dogs:")
 for line2 in lines2:
     print(f"-{line2}")
+
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+#lines = contents.splitlines()
+#for line in lines:
+#    print(line)
+
+for line in contents.splitlines():
+    print(line)
+
+path = Path('pi_million_digits.txt')
+contents = path.read_text()
+
+pi_string = ''
+#lines = contents.splitlines()
+#for line in lines:
+#    pi_string += line.lstrip()
+for line in contents.splitlines():
+    pi_string += line.lstrip()
+print(f"{pi_string[:53]}...")
+print(len(pi_string))
+
+
+from pathlib import Path
+
+path = Path('pi_million_digits.txt')
+contents = path.read_text()
+
+pi_string = ''
+for line in contents.splitlines():
+    pi_string += line.lstrip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
+
