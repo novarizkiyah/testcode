973e06c9520534b0ae2a7d9fe9571ca409b21261
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-27 15:17:04 +0100

    make more simple

--- python-crash-course/bab-10-files-and-exceptions/10-3-simpler-code.py
+++ python-crash-course/bab-10-files-and-exceptions/10-3-simpler-code.py
@@ -9,26 +9,44 @@ for line in contents.splitlines():
 
 from pathlib import Path
 
-path = Path('pi_digits.txt')
+'''First code pi digits'''
+
+path = Path("pi_digits.txt")
 contents = path.read_text()
-
-#lines = contents.splitlines()
-#for line in lines:
-#    print(line)
-
+'''
+lines = contents.splitlines()
+for line in lines:
+    print(line)
+'''
 for line in contents.splitlines():
     print(line)
 
 
-path = Path('pi_million_digits.txt')
+''' Second code pi million digit'''
+path = Path("pi_million_digits.txt")
 contents = path.read_text()
 
 pi_string = ''
-#lines = contents.splitlines()
-#for line in lines:
-#    pi_string += line.lstrip()
+'''
+lines = contents.splitlines()
+for line in lines:
+    pi_string += line.lstrip()
+'''
 for line in contents.splitlines():
     pi_string += line.lstrip()
-print(f"{pi_string[:53]}...")
+print(f"{pi_string[:52]}")
 print(len(pi_string))
 
+'''Last code about is your birthday in pi million digit?'''
+path = Path("pi_million_digits.txt")
+contents = path.read_text()
+
+pi_string = ''
+for line in contents.splitlines():
+    pi_string += line.lstrip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday in the first million digit of pi!")
+else:
+    print("Your birthday does not in the first million digit of pi")
