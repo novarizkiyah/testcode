dc43a7d8ec383ea450d5f6ba80587597de8c5ba2
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-12-28 16:38:20 +0100

    Add while loops in addition

--- python-crash-course/bab-10-files-and-exceptions/10-7-addition-calculator.py
+++ python-crash-course/bab-10-files-and-exceptions/10-7-addition-calculator.py
@@ -2,19 +2,20 @@
 # so the user can continue entering numbers 
 # even if they make a mistake and enter text instead of a number.
 
-while True:
-    print("\n Give me 2 numbers")
-    print("\n Enter q to quit")
-    first_number = input("\n Your first number: ")
-    if first_number == 'q':
-        break
-    second_number = input("\n Your second number: ")
-    if second_number == 'q':
-        break
-    try:
-        sum = int(first_number) + int(second_number)
-    except ValueError:
-        print("\n You must input number!")
-    else:
-        print(f"\n sum : {sum}")
 
+while True:
+    print("\n Please type 'quit' to exit")
+    print("\n Please input 2 numbers")
+    number_one = input("\n What is your number one? ")
+    if number_one == 'quit':
+        break
+    number_two = input("\n What is your second number? ")
+    if number_two == 'quit':
+        break
+
+    try: 
+        sum = int(number_one)+ int(number_two)
+    except ValueError:
+        print("\n Please input numbers!")
+    else:
+        print(f"\n The addition is : {sum}")
