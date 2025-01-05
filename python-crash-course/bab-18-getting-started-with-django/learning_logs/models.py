20d11768764741f819edaca6add61c6ac8bace2a
novarizkiyah <novalailatulrizkiyah@gmail.com>
2025-01-05 20:20:55 +0100

    Edit date

--- python-crash-course/bab-18-getting-started-with-django/learning_logs/models.py
+++ python-crash-course/bab-18-getting-started-with-django/learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Topic(models.Model):
     '''A topic of user learning about'''
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.text
     
