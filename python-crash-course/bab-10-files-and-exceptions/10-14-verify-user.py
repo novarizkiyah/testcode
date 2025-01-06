916bedaeeb1242fa9c88b37762412c0203b0be3a
novarizkiyah <novalailatulrizkiyah@gmail.com>
2025-01-06 15:10:21 +0100

    Add verify user

--- python-crash-course/bab-10-files-and-exceptions/10-14-verify-user.py
+++ python-crash-course/bab-10-files-and-exceptions/10-14-verify-user.py
@@ -12,7 +12,6 @@ from pathlib import Path
 import json
 
 def get_stored_username(path):
-    '''Get stored username if available'''
     if path.exists():
         contents = path.read_text()
         username = json.loads(contents)
@@ -20,17 +19,15 @@ def get_stored_username(path):
     else:
         return None
 def get_new_username(path):
-    '''Prompt for a new username'''
-    username = input("What is your name? ")
+    username = input("what is your username? ")
     contents = json.dumps(username)
     path.write_text(contents)
     return username
 def greet_user():
-    '''Greet user'''
-    path = Path('username.json')
+    path = Path("username.json")
     username = get_stored_username(path)
     if username:
-        correct = input(f"Are you {username}? (y/n) ")
+        correct = input(f"Are you {username}? (y/N) ")
         if correct == 'y':
             print(f"Welcome back, {username}")
         else:
@@ -40,20 +37,3 @@ def greet_user():
         username = get_new_username(path)
         print(f"We'll remember you dear {username}")
 greet_user()
-
-'''
-def greet_user():
-    """Greet the user by name."""
-    path = Path('username.json')
-    username = get_stored_username(path)
-    if username:
-        correct = input(f"Are you {username}? (y/n) ")
-        if correct == 'y':
-            print(f"Welcome back, {username}!")
-            return
-
-    # We got a username, but it's not correct.
-    #   Prompt for a new username.
-    username = get_new_username(path)
-    print(f"We'll remember you when you come back, {username}!")
-'''
