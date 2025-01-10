398eeb23df451c2b1916309c7eda6b0dee291ec4
novarizkiyah <novalailatulrizkiyah@gmail.com>
2025-01-10 10:55:21 +0100

    Add pytest

--- python-crash-course/bab-11-testing-your-code/test_employee_2.py
+++ python-crash-course/bab-11-testing-your-code/test_employee_2.py
@@ -5,10 +5,10 @@ from employee import Employee
 def employee():
     employee = Employee('Nova', 'Rizkiyah', 100000)
     return employee
-def test_give_default_raise():
+def test_give_default_raise(employee):
     employee.give_raise()
     assert employee.salary == 105000
 
-def test_give_custom_raise():
+def test_give_custom_raise(employee):
     employee.give_raise(10000)
     assert employee.salary == 110000
